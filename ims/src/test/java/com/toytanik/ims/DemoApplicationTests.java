fb226cae3bea1a7b1cb13199cce52ec1ac1fbcf4
tuncay turk <tuncayturk.cse@gmail.com>
2020-04-14 13:57:52 +0100

    First Commit - started project today :)

---
+++ ims/src/test/java/com/toytanik/ims/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toytanik.ims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
